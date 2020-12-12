6db3cf5251b8587166df79d8c16f64dc90d8c3d4
Darshana Ariyarathna <darshana.uop@gmail.com>
2020-12-12 16:26:44 +0530

    create visual test case

--- 00 - Properties/01 - JDeltaColors/JDeltaColor/src/test/java/JDeltaColors/JDeltaColorsUI.java
+++ 00 - Properties/01 - JDeltaColors/JDeltaColor/src/test/java/JDeltaColors/JDeltaColorsUI.java
@@ -7,12 +7,56 @@ public class JDeltaColorsUI extends JFrame {
     com.deltaApps.colors.JDeltaColor jDeltaColor;
 
     JPanel panLeft,panTop,panRight,panSouth;
+    JPanel middlePan;
+    JPanel midpanLeft,midpanTop,midpanRight,midpanSouth;
 
     public JDeltaColorsUI(){
         setLayout(new BorderLayout());
 
+        /* panLeft with PRIMARY color */
         panLeft = new JPanel();
-        panLeft.setBackground(jDeltaColor.WARNING);
+        panLeft.setBackground(jDeltaColor.PRIMARY);
         add(panLeft,BorderLayout.WEST);
+
+        /* panTop with SECONDARY color */
+        panTop = new JPanel();
+        panTop.setBackground(jDeltaColor.SECONDARY);
+        add(panTop,BorderLayout.NORTH);
+
+        /* panRight with SUCCESS color */
+        panRight = new JPanel();
+        panRight.setBackground(jDeltaColor.SUCCESS);
+        add(panRight,BorderLayout.EAST);
+
+        /* panSouth with SUCCESS color */
+        panSouth = new JPanel();
+        panSouth.setBackground(jDeltaColor.DANGER);
+        add(panSouth,BorderLayout.SOUTH);
+
+        /* add a middle panel */
+        middlePan = new JPanel(new BorderLayout());
+        add(middlePan,BorderLayout.CENTER);
+
+        /* midpanLeft with WARNING color */
+        midpanLeft = new JPanel();
+        midpanLeft.setBackground(jDeltaColor.WARNING);
+        middlePan.add(midpanLeft,BorderLayout.WEST);
+
+        /* midpanLeft with INFO color */
+        midpanTop = new JPanel();
+        midpanTop.setBackground(jDeltaColor.INFO);
+        middlePan.add(midpanTop,BorderLayout.NORTH);
+
+        /* midpanRight with INFO color */
+        midpanRight = new JPanel();
+        midpanRight.setBackground(jDeltaColor.LIGHT);
+        middlePan.add(midpanRight,BorderLayout.EAST);
+
+        /* midpanRight with INFO color */
+        midpanSouth = new JPanel();
+        midpanSouth.setBackground(jDeltaColor.DARK);
+        middlePan.add(midpanSouth,BorderLayout.SOUTH);
+
+
     }
 }
