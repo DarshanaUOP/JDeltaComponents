bf722d80d1bd0b6aef22ee12a01d0b5af7f09aa4
Darshana Ariyarathna <darshana.uop@gmail.com>
2021-02-18 19:49:00 +0530

    badge update

--- 01 - Badges/01 - Badge/Badges/src/test/java/JDeltaBadge/JDeltaBadgeHolder.java
+++ 01 - Badges/01 - Badge/Badges/src/test/java/JDeltaBadge/JDeltaBadgeHolder.java
@@ -2,11 +2,14 @@ package JDeltaBadge;
 
 import com.deltaApps.JDeltaBadge.JDeltaBadge;
 import com.deltaApps.JDeltaColors.JDeltaColor;
+import com.deltaApps.JDeltaFont.JDeltaFonts;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+
+import static java.lang.Thread.sleep;
 
 /**
  * Owner : Darshana Ariyarathna. 2020-12-11
@@ -14,7 +17,7 @@ import java.awt.event.MouseEvent;
 /*
 * This Class is only for create holder for jDeltaBadge
 */
-public class JDeltaBadgeHolder extends JFrame {
+public class JDeltaBadgeHolder<intArray> extends JFrame {
     /* get jDeltaBadge Component */
     private JDeltaBadge jDeltaBadgeNotification, jDeltaBadgeCustomSecondary;
 
@@ -25,6 +28,11 @@ public class JDeltaBadgeHolder extends JFrame {
     private JDeltaBadge firstTestBadge,secondTestBadge;
 
     int clickCount = 0 ;
+    int testCase = 0;
+
+    JDeltaColor itemColor = JDeltaColor.LIGHT;
+    JDeltaFonts itemFont = JDeltaFonts.JDELTA_FONTS_ARIAL_S;
+    int[] testArray = new int[20];
     /* Constructor of the class */
     public JDeltaBadgeHolder(){
         setLayout(new BorderLayout());
@@ -73,7 +81,12 @@ public class JDeltaBadgeHolder extends JFrame {
         /* test get/set properties */
         testPanel = new JPanel(new GridBagLayout());
         outputLabel = new JLabel("Output label");
-        firstTestBadge = new JDeltaBadge("badge 1",1);
+        firstTestBadge = new JDeltaBadge("JDeltaBadge","New");
+        firstTestBadge.setBackgroundColor(JDeltaColor.PRIMARY);
+        firstTestBadge.setPrimaryForegroundColor(JDeltaColor.LIGHT);
+        firstTestBadge.setSecondaryBackgroundColor(JDeltaColor.LIGHT);
+        firstTestBadge.setSecondaryForegroundColor(JDeltaColor.PRIMARY);
+
         secondTestBadge = new JDeltaBadge("badge 2","new");
         instructionLabel = new JLabel("Instructions Label");
 
@@ -81,7 +94,7 @@ public class JDeltaBadgeHolder extends JFrame {
         gb.gridy = 0;
         gb.weightx = 2;
         gb.weighty = 1;
-        testPanel.add(outputLabel,gb);
+        testPanel.add(instructionLabel,gb);
 
         gb.gridy = 1;
         gb.weightx = 1;
@@ -93,8 +106,49 @@ public class JDeltaBadgeHolder extends JFrame {
         gb.gridx = 0;
         gb.gridy = 2;
         gb.weightx = 2;
-        testPanel.add(instructionLabel,gb);
+        testPanel.add(outputLabel,gb);
 
         add(testPanel,BorderLayout.CENTER);
+
+        firstTestBadge.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                switch (testCase){
+                    case 0: /* set/get background */
+                        new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                instructionLabel.setText("Executing : setBackgroundColor");
+                            }
+                        }).run();
+//                        instructionLabel.setText("Executing : setBackgroundColor");
+                        waitToRead();
+                        secondTestBadge.setBackground(JDeltaColor.WARNING);
+                        waitToRead();
+                        itemColor = secondTestBadge.getBackgroundColor();
+                        if(itemColor != JDeltaColor.WARNING){
+                            System.out.println("Error : color set to item is not returned back");
+                            testArray[testCase] = 1;
+                        }else {
+                            System.out.println();
+                            testArray[testCase] = 0;
+                        }
+                        break;
+
+                    case 1:
+
+                        break;
+                }
+                testCase++;
+            }
+        });
+    }
+
+    public void waitToRead(){
+        try {
+            sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
