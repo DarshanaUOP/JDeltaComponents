3ea20666e1eef91c4a4bbeb2c52b29a55926cbb6
Darshana Ariyarathna <darshana.uop@gmail.com>
2021-03-22 00:37:17 +0530

    Test case defined but wrong colors appended

--- src/test/java/com/deltaApps/components/JDeltaBadgeTest.java
+++ src/test/java/com/deltaApps/components/JDeltaBadgeTest.java
@@ -8,12 +8,29 @@ import java.awt.*;
 public class JDeltaBadgeTest {
     @Test
     public void JDeltaButtonWithButtonType(){
+        JFrame backFrame;
         JPanel backgroundPanel;
         JLabel nameLabel;
         JDeltaBadge jDeltaBadgeWithButton;
 
+        backFrame = new JFrame();
         backgroundPanel = new JPanel(new FlowLayout());
         nameLabel = new JLabel("Testing button type");
-        jDeltaBadgeWithButton = new JDeltaBadge("Test Badge","1",JDeltaBadge.PRIMARY_BUTTON)
+        jDeltaBadgeWithButton = new JDeltaBadge("Test Badge","1",JDeltaBadge.PRIMARY_BUTTON);
+
+        backgroundPanel.add(nameLabel);
+        backgroundPanel.add(jDeltaBadgeWithButton);
+
+        backFrame.add(backgroundPanel);
+        backFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        backFrame.setVisible(true);
+        backFrame.setSize(500,300);
+        backFrame.setEnabled(true);
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 }
