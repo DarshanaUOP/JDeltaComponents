55b0c6a71612fbd65eaa5ce19a9f6233e76d8698
Darshana Ariyarathna <darshana.uop@gmail.com>
2021-03-22 19:20:04 +0530

    Create jdeltaBadgeViewr for visually inspect created jDeltaBadge

--- src/test/java/com/deltaApps/components/JDeltaBadgeTest.java
+++ src/test/java/com/deltaApps/components/JDeltaBadgeTest.java
@@ -6,31 +6,82 @@ import javax.swing.*;
 import java.awt.*;
 
 public class JDeltaBadgeTest {
-    @Test
-    public void JDeltaButtonWithButtonType(){
-        JFrame backFrame;
-        JPanel backgroundPanel;
-        JLabel nameLabel;
-        JDeltaBadge jDeltaBadgeWithButton;
+    /**
+     * JDeltaBadge viewer for use in all test cases in case of visual inspection
+     */
+    JDeltaButtonViewer jDeltaBadgeViewer = new JDeltaButtonViewer();
 
-        backFrame = new JFrame();
+
+}
+
+class JDeltaButtonViewer extends JFrame{
+    /**
+     * This class is for display jDeltaBadges which is created by particular test cases.
+     * by using this, tester can visually inspect his implementation
+     */
+    private JPanel backgroundPanel;
+    private JLabel nameLabel = new JLabel();
+    private JDeltaBadge jDeltaBadgeObj;
+
+    /**
+     * Default constructor of the JDeltaButtonViewer
+     * @since v1.0.1
+     */
+    public JDeltaButtonViewer(){
+        this("",null);
+    }
+
+    /**
+     * Detailed constructor of the JDeltaButtonViewer
+     * @param badgeName Name of the badge
+     * @param badge Created JDeltaBadge by the test
+     * @since v1.0.1
+     */
+    public JDeltaButtonViewer(String badgeName,JDeltaBadge badge){
+
+        nameLabel.setText(badgeName);
+        jDeltaBadgeObj = badge;
+
         backgroundPanel = new JPanel(new FlowLayout());
-        nameLabel = new JLabel("Testing button type");
-        jDeltaBadgeWithButton = new JDeltaBadge("Test Badge","1",JDeltaBadge.PRIMARY_BUTTON);
 
         backgroundPanel.add(nameLabel);
-        backgroundPanel.add(jDeltaBadgeWithButton);
+        backgroundPanel.add(jDeltaBadgeObj);
 
-        backFrame.add(backgroundPanel);
-        backFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        backFrame.setVisible(true);
-        backFrame.setSize(500,300);
-        backFrame.setEnabled(true);
+        add(backgroundPanel);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+
+    /**
+     * setter for the badge name
+     * @param BadgeName
+     * @since v1.0.1
+     */
+    public void setBadgeName(String BadgeName){
+        this.nameLabel.setText(BadgeName);
+    }
+
+    /**
+     * setter for the JDeltaBadge object
+     * @param newJDeltaBadge jDeltaBadge for set in the viewer
+     * @since v1.0.1
+     */
+    public void setJDeltaBadgeObj(JDeltaBadge newJDeltaBadge){
+        this.jDeltaBadgeObj = newJDeltaBadge;
+    }
+
+    /**
+     * This method is for show the JDeltaButtonViewer
+     * @since v1.0.1
+     */
+    public void showBadge(){
+        setVisible(true);
+        setSize(500,300);
+        setEnabled(true);
         try {
-            Thread.sleep(10000);
+            Thread.sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
     }
 }
